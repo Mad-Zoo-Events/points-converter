b1e220437a131f8ae69d0f3c8b53d64291677949
Adrian Riedel <adrian.riedel@outlook.com>
2021-05-09 14:19:44 +0200

    fetch points and upload markers automatically
    - communicate to configurable server through scp
    - convert codebase to ES6 module

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ This utility converts Aurora points to BlueMap markers file
 
 ## How To Use
 
-- place Aurora's `points.yaml` file here
+- make sure you have access to the desired server and a corresponding private ssh key ready
+- create a `conf.json` file in this directory that looks like this:
+    ````
+    {
+        "host": "12.345.67.89",
+        "privateKeyPath": "/some/key.pem"
+    }
+    ````
 - run `yarn convert`
-- upload the resulting `markers.json` to the server
--- index.js
+++ index.js
@@ -1,45 +1,101 @@
-const yaml = require('js-yaml');
-const fs = require('fs');
+import {readFileSync, writeFileSync, rmSync} from "fs";
+import {load} from "js-yaml";
+import scp from "node-scp";
 
-const pointsYaml = yaml.load(fs.readFileSync('points.yaml', 'utf8'));
-const pointsObjKeys = Object.keys(pointsYaml.points);
-const pointsObjValues = Object.values(pointsYaml.points);
+const connect = async () => {
+    const conf = JSON.parse(readFileSync("./conf.json"));
 
-const points = [];
+    const hotg1 = {
+        host: conf.host,
+        port: 22,
+        username: "ec2-user",
+        privateKey: readFileSync(conf.privateKeyPath)
+    };
 
-pointsObjKeys.forEach((k, i) => {
-    const {x, y, z} = pointsObjValues[i].vector;
-    points.push({
-        id: `point${k}`,
-        type: "poi",
-        map: "stage",
-        position: {
-            x,
-            y,
-            z
-        },
-        minDistance: 0.0,
-        maxDistance: 100000.0,
-        label: `Point ${k}`,
-        newTab: true,
-        icon: "assets/poi.svg",
-        iconAnchor: {
-            "x": 25,
-            "y": 45
-        }
-    });
-});
-
-const result = {
-    markerSets: [
-        {
-            id: "markers",
-            label: "Markers",
-            toggleable: true,
-            defaultHide: false,
-            marker: points
-        }
-    ]
+    return await scp(hotg1);
 };
 
-console.log(JSON.stringify(result));
+const fetchPoints = async (client) => {
+    try {
+        await client.downloadFile("plugins/Aurora/points.yml", "points.yml");
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+const uploadMarkers = async (client) => {
+    try {
+        await client.uploadFile("markers.json", "bluemap/web/data/markers.json");
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+const convertToMarkers = (pointsYaml) => {
+    const pointsObjKeys = Object.keys(pointsYaml.points);
+    const pointsObjValues = Object.values(pointsYaml.points);
+    const points = [];
+
+    pointsObjKeys.forEach((k, i) => {
+        const {x, y, z} = pointsObjValues[i].vector;
+        points.push({
+            id: `point${k}`,
+            type: "poi",
+            map: "stage",
+            position: {
+                x,
+                y,
+                z
+            },
+            minDistance: 0.0,
+            maxDistance: 100000.0,
+            label: `Point ${k}`,
+            newTab: true,
+            icon: "assets/poi.svg",
+            iconAnchor: {
+                "x": 25,
+                "y": 45
+            }
+        });
+    });
+
+    const result = {
+        markerSets: [
+            {
+                id: "markers",
+                label: "Markers",
+                toggleable: true,
+                defaultHide: false,
+                marker: points
+            }
+        ]
+    };
+
+    return JSON.stringify(result);
+};
+
+const main = async () => {
+    const client = await connect();
+
+    try {
+        await fetchPoints(client);
+
+        const pointsYaml = load(readFileSync("points.yml", "utf8"));
+        const markersJson = convertToMarkers(pointsYaml);
+
+        writeFileSync("markers.json", markersJson);
+
+        await uploadMarkers(client);
+
+        rmSync("points.yml");
+        rmSync("markers.json");
+    } catch (error) {
+        console.error(error);
+    }
+
+    client.close();
+};
+
+main().then(() => {
+    console.log("done!");
+});
